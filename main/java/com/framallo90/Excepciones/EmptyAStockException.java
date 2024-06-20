5c2460d32df14ac61a2bb7d4c34d2bb8d91794d8
piragna1 <gonzalovarelaalagna@gmail.com>
2024-06-20 18:11:13 -0300

    Delete main directory REPETIDO

--- main/java/com/framallo90/Excepciones/EmptyAStockException.java
+++
@@ -1,7 +0,0 @@
-package com.framallo90.Excepciones;
-
-public class EmptyAStockException extends RuntimeException{
-    public EmptyAStockException(String message) {
-        super(message);
-    }
-}
--- main/java/com/framallo90/Excepciones/InvalidIdNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.framallo90.Excepciones;
-
-public class InvalidIdNotFound extends Exception{
-    public InvalidIdNotFound(String message) {
-        super(message);
-    }
-}
